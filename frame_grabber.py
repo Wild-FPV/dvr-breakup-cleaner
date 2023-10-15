125160531b3b2df02a62309060fde43ce98360db
Wild-FPV <102388595+Wild-FPV@users.noreply.github.com>
2023-10-14 23:36:33 -0400

    Add frame grabber script
    "script 1" in project description. Now have sample frames in a folder with "breakup" and "clean" folders created.

---
+++ frame_grabber.py
@@ -0,0 +1,56 @@
+import cv2
+import os
+import random
+
+dvr_filename = 'DVRforGrabber.avi'
+#change to actual video filename
+
+output_folder_name = 'frame samples'
+
+sample_percent = 5
+
+if not os.path.exists(output_folder_name):
+    os.mkdir(output_folder_name)
+    
+if not os.path.exists("breakup"):
+    os.mkdir("breakup")
+    
+if not os.path.exists("clean"):
+    os.mkdir("clean")
+    
+
+dvr = cv2.VideoCapture(dvr_filename)
+
+if not dvr.isOpened():
+    print("Video file could not be opened")
+    exit()
+    
+    
+frame_count = int(dvr.get(cv2.CAP_PROP_FRAME_COUNT))
+
+frames_removed = int(frame_count * (sample_percent / 100))
+
+sample_frame_numbers = random.sample(range(frame_count),frames_removed)
+
+frame_number = 0
+
+while True:
+    ret, frame = dvr.read()
+    
+    
+    if not ret:
+        break
+    
+    if frame_number in sample_frame_numbers:
+        frame_filename = os.path.join(output_folder_name, f'frame_{frame_number:04d}.jpg')
+        cv2.imwrite(frame_filename,frame)
+        
+    
+    frame_number += 1
+    
+    
+dvr.release()
+
+print(f'{frames_removed} frames grabbed and saved to {output_folder_name}')
+
+        
